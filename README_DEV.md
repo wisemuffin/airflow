984f5e304f420e67d64c978f9565660fcbc70b3b
David Griffiths <davidgg777@hotmail.com>
2021-01-26 21:42:15 +1100

    airflow test via vscode example added to readme

--- README_DEV.md
+++ README_DEV.md
@@ -2,12 +2,14 @@
 
 [astronamer.io](https://www.astronomer.io/)
 
+[alternative example via docker-compose](https://github.com/jamesang17/airflow-app/blob/e54a4f6f2dd142086bf3cdfb04821744b4c757c4/docker-compose.yml)
+
 ```bash
 astro dev start
 ```
 
 ## to reset metadata
-
+note these astro commands have been depricated
 ```bash
 astro airflow kill
 
@@ -18,3 +20,35 @@ astro airflow start
 
 astronomer cli - not working - https://www.astronomer.io/docs/cloud/stable/develop/customize-image
 instead exec into container and add via /utils/connections.py
+
+
+## debug when in the the astro container
+*doesnt work in local if you dont have a localy running airflow scheduler.
+
+first [remote-containers: Attach to running container](https://www.youtube.com/watch?v=qCCj7qy72Bg) into the running scheduler.
+
+launch.json run airflow dag test
+
+```json
+{
+    // Use IntelliSense to learn about possible attributes.
+    // Hover to view descriptions of existing attributes.
+    // For more information, visit: https://go.microsoft.com/fwlink/?linkid=830387
+    "version": "0.2.0",
+    "configurations": [
+        {
+            "name": "Airflow Test",
+            "type": "python",
+            "request": "launch",
+            "program": "/usr/local/bin/airflow",
+            "console": "integratedTerminal",
+            "args": [
+                "dags",
+                "test",
+                "bashenv",
+                "2020-01-12"
+            ]
+        }
+    ]
+}
+```
--- dags/tweet_analyzer/tweet_analyzer.py
+++ dags/tweet_analyzer/tweet_analyzer.py
@@ -12,6 +12,8 @@ import collections
 import logging
 import json
 
+S3_BUCKET = 'tweet-analyzer-dg'
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -19,14 +21,13 @@ default_args = {
     'email': [''],
     'email_on_failure': False,
     'email_on_retry': False,
-    's3_bucket': 'tweet-analyzer-dg',
+    's3_bucket': S3_BUCKET,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
     'schedule_interval': '@daily'
 }
 
 timestamp = '{{ ts_nodash }}'
-
 
 with DAG(
     dag_id='tweet_analyzer',
@@ -38,38 +39,41 @@ with DAG(
 
     tweets_to_s3 = TweetsToS3Operator(
         task_id='tweets_to_s3',
-        # topic='{{ dag_run.conf["topic"] }}',
+        topic='{{ dag_run.conf["topic"] }}',
         # description='Writes tweets about a certain topic to S3',
         max_tweets=100,
         s3_key='tweet_data.' + timestamp
     )
 
-    # etl_tweets = S3FileTransformOperator(
-    #     task_id='etl_tweets',
-    #     description='cleans the tweet jsons pulled',
-    #     source_s3_key='s3://j17devbucket/tweet_data.' + timestamp,
-    #     dest_s3_key='s3://j17devbucket/cleaned_tweet_data.' + timestamp,
-    #     replace=True,
-    #     transform_script='scripts/etl/clean_tweets_pipeline.py'
-    # )
+    etl_tweets = S3FileTransformOperator(
+        task_id='etl_tweets',
+        # description='cleans the tweet jsons pulled',
+        source_s3_key=f's3://{S3_BUCKET}/tweet_data.' + timestamp,
+        dest_s3_key='s3://{S3_BUCKET}/cleaned_tweet_data.' + timestamp,
+        replace=True,
+        transform_script='dags/tweet_analyzer/python/clean_tweets_pipeline.py'
+    )
 
-    # get_sentiment = S3FileTransformOperator(
-    #     task_id='get_sentiment',
-    #     description='Get sentiment of tweets',
-    #     source_s3_key='s3://j17devbucket/cleaned_tweet_data.' + timestamp,
-    #     dest_s3_key='s3://j17devbucket/analyzed_tweet_data_' + timestamp + '.json',
-    #     replace=True,
-    #     transform_script='scripts/nlp/sentiment_analysis.py'
-    # )
+    get_sentiment = S3FileTransformOperator(
+        task_id='get_sentiment',
+        # description='Get sentiment of tweets',
+        source_s3_key='s3://{S3_BUCKET}/cleaned_tweet_data.' + timestamp,
+        dest_s3_key='s3://{S3_BUCKET}/analyzed_tweet_data_' + \
+        timestamp + '.json',
+        replace=True,
+        transform_script='dags/tweet_analyzer/python/sentiment_analysis.py'
+    )
 
-    # summarize_sentiment = S3FileTransformOperator(
-    #     task_id='summarize_sentiment',
-    #     description='Summarize sentiment of topic',
-    #     source_s3_key='s3://j17devbucket/analyzed_tweet_data_' + timestamp + '.json',
-    #     dest_s3_key='s3://j17devbucket/sentiment_results_' + timestamp + '.json',
-    #     replace=True,
-    #     transform_script='scripts/etl/summarize_results.py'
-    # )
+    summarize_sentiment = S3FileTransformOperator(
+        task_id='summarize_sentiment',
+        # description='Summarize sentiment of topic',
+        source_s3_key='s3://{S3_BUCKET}/analyzed_tweet_data_' + \
+        timestamp + '.json',
+        dest_s3_key='s3://{S3_BUCKET}/sentiment_results_' + \
+        timestamp + '.json',
+        replace=True,
+        transform_script='dags/tweet_analyzer/python/summarize_results.py'
+    )
 
     # sentiment_results_to_dynamoDB = S3ToDynamoDBOperator(
     #     task_id='write_sentiment_to_dynamoDB',
@@ -77,14 +81,14 @@ with DAG(
     #     table_name='sentiment-results',
     #     table_keys=["topic","timestamp","maxNegText","maxPosText","sentiment"],
     #     region_name='us-east-1',
-    #     s3_key='s3://j17devbucket/sentiment_results_' + timestamp + '.json',
+    #     s3_key='s3://{S3_BUCKET}/sentiment_results_' + timestamp + '.json',
     #     json_key='results'
     # )
 
     # clean_up = S3DeleteObjectsOperator(
     #     task_id='clean_up_s3',
     #     description='Clean up files on s3',
-    #     bucket='j17devbucket',
+    #     bucket={S3_BUCKET},
     #     keys=['tweet_data.' + timestamp,
     #        'cleaned_tweet_data.' + timestamp,
     #        'analyzed_tweet_data_' + timestamp + '.json',
@@ -93,4 +97,4 @@ with DAG(
     # )
 
     # tweets_to_s3 >> etl_tweets >> get_sentiment >> summarize_sentiment >> sentiment_results_to_dynamoDB >> clean_up
-    tweets_to_s3
+    tweets_to_s3 >> etl_tweets >> get_sentiment >> summarize_sentiment
